e9d225cece01ea1c54fb3e73a5ca4a64b5aebb48
Steven Varela <stevenvarela51@gmail.com>
2023-09-26 13:33:40 -0400

    :construction: sidebar styles

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 // src/components/Layout.tsx
-import Link from 'next/link';
 import React from 'react';
+import Sidebar from './Sidebar';
 
 type LayoutProps = {
   children: JSX.Element
@@ -10,19 +10,11 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="uk-flex uk-flex-column uk-height-viewport">
       <div className="uk-grid-divider uk-child-width-expand@s" data-uk-grid>
         {/* Sidebar */}
-        <aside className="uk-width-auto@m">
-          <h3 className="uk-margin-remove-bottom">Menu</h3>
-          <ul className="uk-nav uk-nav-default uk-margin-remove-top">
-            <li>
-              <a href="#">Manage Categories</a>
-            </li>
-            <li>
-              <a href="#">Manage Content</a>
-            </li>
-          </ul>
-        </aside>
+        <Sidebar />
         {/* Content */}
-        <main className="uk-width-expand@m">{children}</main>
+        <main className="uk-width-expand@m">
+          {children}
+        </main>
       </div>
     </div>
   );
--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import Link from 'next/link';
 import SidebarData from '@/data/SidebarData.json'
 
 const Sidebar: React.FC = () => {
   const { data } = SidebarData
   return (
     <aside className="uk-width-1-4@m uk-visible@m">
-      <ul className="uk-nav uk-nav-default">
+      <ul data-uk-accordion>
         {
-          data.map((item) => (
-            <li key={item.id}>
-              <a href={item.link}>
-                {item.title}
-              </a>
-            </li>
-          ))
+          data.map((element, i) => {
+            return (
+              <li key={element.label} className={i === 1 ? 'uk-open' : ''}>
+                <a className="uk-accordion-title accordion-title-sidebar" href="#">
+                  {element.label}
+                </a>
+                <div className="uk-accordion-content accordion-content-sidebar">
+                  <ul className='uk-list'>
+                    {
+                      element.items.map(item => {
+                        return (
+                          <li key={item.title}>
+                            <Link href={item.href} className='uk-link-heading'>
+                              {item.title}
+                            </Link>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </div>
+              </li>
+            )
+          })
         }
       </ul>
     </aside>
