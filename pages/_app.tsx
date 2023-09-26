4be4595fa1429dadfa5c0041b8b1703f4404bcf5
Steven Varela <stevenvarela51@gmail.com>
2023-09-26 17:09:27 -0400

    :wrench: uikit icons - layout and promise

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,36 @@
 import type { AppProps } from 'next/app'
 import "uikit/dist/css/uikit.min.css";
-import "uikit/dist/css/uikit-core.min.css";
-import "uikit/dist/js/uikit.min.js";
-import "uikit/dist/js/uikit-icons.min.js";
 import '@/components/Spinner/Spinner.css'
 import '@/styles/globals.css'
 import { SpinnerProvider } from '@/context/SpinnerContext'
 import Header from '@/components/Layout/Header';
+import Icons from 'uikit/dist/js/uikit-icons';
+import { useCallback, useEffect } from 'react';
 
+// loads the Icon plugin
 export default function App({ Component, pageProps }: AppProps) {
+  const importUIkit = useCallback(async () => {
+    const UIkit = (await import('uikit')).default;
+    UIkit.use(Icons);
+  }, []);
+
+  useEffect(() => {
+    importUIkit();
+  }, [importUIkit]);
   return (
-    <SpinnerProvider>
-      <div className="root">
-        <div className='header-container'>
-          <Header />
-        </div>
-        <div className="container">
-          <div className='sub-container'>
-            <Component {...pageProps} />
+    <>
+      <SpinnerProvider>
+        <div className="root">
+          <div className='header-container'>
+            <Header />
+          </div>
+          <div className="container">
+            <div className='sub-container'>
+              <Component {...pageProps} />
+            </div>
           </div>
         </div>
-      </div>
-    </SpinnerProvider>
+      </SpinnerProvider>
+    </>
   )
 }
