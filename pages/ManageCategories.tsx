d11d8d3332501619e386c7ef383d2f0bab91f809
Steven Varela <stevenvarela51@gmail.com>
2023-09-26 18:13:42 -0400

    :construction: item card header - table

--- pages/ManageCategories.tsx
+++ pages/ManageCategories.tsx
@@ -8,6 +8,24 @@ function ManageCategories() {
   return (
     <Layout>
       <div className="uk-card uk-card-default uk-card-hover uk-card-body custom-card">
+        <div className="uk-card-header">
+          <div className="uk-grid-small uk-flex-middle" data-uk-grid>
+            <div className="uk-width-expand">
+              <h4 className="uk-card-title uk-margin-remove-bottom uk-text-uppercase uk-text-center">Manage Content categories</h4>
+            </div>
+          </div>
+        </div>
+        <div className="uk-child-width-expand@s uk-text-center" data-uk-grid>
+          <div>
+            <div className="uk-card uk-card-default uk-card-body">Item</div>
+          </div>
+          <div>
+            <div className="uk-card uk-card-default uk-card-body">Item</div>
+          </div>
+          <div>
+            <div className="uk-card uk-card-default uk-card-body">Item</div>
+          </div>
+        </div>
         <h3 className="uk-card-title">Default</h3>
         <table className="uk-table uk-table-divider uk-table-striped custom-table">
           <thead>
@@ -18,21 +36,24 @@ function ManageCategories() {
               <th>Edit</th>
               <th></th>
             </tr>
-
-
           </thead>
           <tbody>
             {
               data.map((item) => (
                 <tr key={item.id}>
-                  <td>{item.name}</td>
+                  <td >{item.name}</td>
                   <td className='uk-text-capitalize'>
                     <span className={(item.display === 'hidden' ? 'red' : 'green') + '-circle'}></span>
                     {item.display}
                   </td>
-                  <td>{item.price}</td>
+                  <td>
+
+                  </td>
                   <td>
                     <Link href={'/edit/' + item.id} data-uk-icon="icon: pencil"></Link>
+                  </td>
+                  <td>
+                    <Link href={'/delete/' + item.id} data-uk-icon="icon: trash"></Link>
                   </td>
                 </tr>
               ))
