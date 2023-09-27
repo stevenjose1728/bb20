94c170d7fdb2b965f80b4d63aecca7d4b7d04c47
Steven Varela <stevenvarela51@gmail.com>
2023-09-27 13:57:55 -0400

    :wrench: render categories table

--- pages/ManageCategories.tsx
+++ pages/ManageCategories.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import CategoriesData from '@/data/CategoriesData.json'
 import Layout from '@/components/Layout/Layout'
 import Link from 'next/link'
+import { SpinnerContext } from '@/context/SpinnerContext';
 
 function ManageCategories() {
-  const { data } = CategoriesData
+  const { startLoading, stopLoading } = useContext(SpinnerContext);
+
+  const [display, setDisplay] = useState<string>('');
+  const [filterName, setFilterName] = useState<string>('');
+  const displayOptions = [
+    {
+      label: 'show all',
+      value: ''
+    },
+    {
+      label: 'on display',
+      value: 'on display',
+    },
+    {
+      label: 'hidden',
+      value: 'hidden',
+    },
+  ];
+
+  const { data } = CategoriesData;
+  const handleDisplayChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    setDisplay(event.target.value);
+  }
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setFilterName(event.target.value);
+  }
+  const handleResetForm = () => {
+    startLoading();
+    setFilterName('');
+    setDisplay('');
+    stopLoading();
+  }
   return (
     <Layout>
       <div className="uk-card uk-card-default uk-card-hover uk-card-body custom-card">
@@ -17,13 +51,65 @@ function ManageCategories() {
         </div>
         <div className="uk-child-width-expand@s uk-text-center" data-uk-grid>
           <div>
-            <div className="uk-card uk-card-default uk-card-body">Item</div>
+            <div className="">
+              <label className="uk-form-label" htmlFor="displaySelect">Filter By</label>
+              <div className="uk-form-controls">
+                <select
+                  id="displaySelect"
+                  value={display}
+                  className='uk-text-capitalize uk-select'
+                  onChange={handleDisplayChange}
+                >
+                  {
+                    displayOptions.map(element => {
+                      return (
+                        <option
+                          key={element.label}
+                          value={element.value}
+                        >
+                          {element.label}
+                        </option>
+                      )
+                    })
+                  }
+                </select>
+              </div>
+            </div>
           </div>
           <div>
-            <div className="uk-card uk-card-default uk-card-body">Item</div>
+            <div className="">
+              <label className="uk-form-label" htmlFor="search-by-name">Search by Name</label>
+              <div className="uk-form-controls">
+                <input
+                  className="uk-input"
+                  type="text"
+                  id="search-by-name"
+                  value={filterName}
+                  onChange={handleNameChange}
+                />
+                <button
+                  className="uk-button uk-button-secondary uk-button-small"
+                >
+                  Go
+                </button>
+              </div>
+            </div>
           </div>
-          <div>
-            <div className="uk-card uk-card-default uk-card-body">Item</div>
+          <div
+            className="uk-flex uk-justify-center uk-align-items-center"
+            style={{
+              justifyContent: 'center'
+            }}
+          >
+            <div
+            >
+              <button
+                className="uk-button uk-button-text"
+                onClick={() => handleResetForm()}
+              >
+                Reset
+              </button>
+            </div>
           </div>
         </div>
         <h3 className="uk-card-title">Default</h3>
