8aa243acabe5ad91d40e9c41dd634d6a89869997
Steven Varela <stevenvarela51@gmail.com>
2023-09-29 16:54:33 -0400

    :wrench: services to receive subcategoreis & interior sub-categories

--- services/CategoryService.ts
+++ services/CategoryService.ts
@@ -17,6 +17,38 @@ class CategoryService {
     });
 
   }
+  static async getSubCategories(categoryId: number) {
+    return new Promise((resolve, reject) => {
+      axios.get('/ManageCategories/getsubcategoriesbycategoryid/' + categoryId).then(
+        ({ data }) => {
+          // http success
+          resolve(data);
+        },
+        ({ response }) => {
+          const { data } = response;
+          // http failed
+          reject(data);
+        }
+      );
+    });
+
+  }
+  static async getInteriorSubCategories(subCategoryId: number) {
+    return new Promise((resolve, reject) => {
+      axios.get('/ManageCategories/getinteriorsubcategoriesbysubcategoryid/' + subCategoryId).then(
+        ({ data }) => {
+          // http success
+          resolve(data);
+        },
+        ({ response }) => {
+          const { data } = response;
+          // http failed
+          reject(data);
+        }
+      );
+    });
+
+  }
 }
 
 export default CategoryService;
