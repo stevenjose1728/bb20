17e1661a004f5c3266f51678a561eafd1a9e6abe
Steven Varela <stevenvarela51@gmail.com>
2023-09-29 13:25:25 -0400

    :wrench: add .env & axios config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_URL_BASE: process.env.API_URL_BASE,
+  },
 }
 
 module.exports = nextConfig
