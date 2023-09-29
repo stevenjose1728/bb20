634ac76113105d990ce7fb8acb122953f65e433f
Steven Varela <stevenvarela51@gmail.com>
2023-09-29 16:53:29 -0400

    :wrench: models of subcategories & interior sub-categories

---
+++ models/InteriorSubCategory.ts
@@ -0,0 +1,17 @@
+type InteriorSubCategory = {
+  interiorsubcategoryId: number
+  name: string
+  catLandingDescription: string
+  headline: string
+  catLandingDescriptionMarkup: string
+  descriptionMarkup: string
+  externalUrlActive: boolean
+  prettyUrl: string
+  externalUrl: string
+  fileOrig: string
+  static: boolean
+  onDisplay: boolean
+  subcategoryId: number
+}
+
+export default InteriorSubCategory
---
+++ models/SubCategory.ts
@@ -0,0 +1,17 @@
+type SubCategory = {
+  subcategoryId: number
+  name: string
+  catLandingDescription: string
+  headling: string
+  catLandingDescriptionMarkup: string
+  descriptionMarkup: string
+  externalUrlActive: string
+  prettyUrl: string
+  externalUrl: string
+  fileOrig: string
+  static: boolean
+  onDisplay: boolean
+  categoryId: number
+}
+
+export default SubCategory;
