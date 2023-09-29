b4b1efcd469118d3c992353c5d8a788a539e045c
Steven Varela <stevenvarela51@gmail.com>
2023-09-29 13:27:24 -0400

    :wrench: image uploader component

---
+++ components/ImageUploader.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef, ChangeEvent } from 'react';
+
+interface ImageUploaderProps {
+  onImageUpload: (base64Image: string | null) => void;
+}
+
+const ImageUploader: React.FC<ImageUploaderProps> = ({ onImageUpload }) => {
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64Image = reader.result as string;
+        setPreviewImage(base64Image);
+        onImageUpload(base64Image);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const openImageHandler = () => fileInputRef.current?.click();
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
+        style={{ display: 'none' }}
+        ref={(input) => (fileInputRef.current = input)}
+      />
+      <p className='uk-text-bold'>Select Thumbnail <span className='uk-text-small uk-text-light'>Thumbnail is only used on category landing pages.</span></p>
+      <div>
+        <img
+          src={previewImage || '/default-image.jpg'}
+          alt="Preview"
+          width="200"
+          height='200'
+          onClick={() => openImageHandler()}
+        />
+      </div>
+      <div className='uk-margin-left'>
+        <button onClick={() => openImageHandler()}>Seleccionar Imagen</button>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUploader;
